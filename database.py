56994430e2da815de45e863a587156780e1ccebe
unknown <Arslan Ahmad>
2026-07-17 17:49:22 +0500

    Blog Api firts commit

---
+++ database.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()  # Load environment variables from .env file
+
+DATABASE_URL = os.getenv("DATABASE_URL")  # Get database URL from environment variables
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Text
+from database import Base
+
+
+
+class Blog(Base):
+    __tablename__ = "blogs"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(255), nullable=False)
+    content = Column(Text, nullable=False)
+    author = Column(String(100), nullable=False)
