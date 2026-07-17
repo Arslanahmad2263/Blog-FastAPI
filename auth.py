5f009ff7adb76c177834e8c3e58c956d8a3d173c
unknown <Arslan Ahmad>
2026-07-17 21:23:45 +0500

    implement authentication required

---
+++ auth.py
@@ -0,0 +1,33 @@
+from jose import JWTError, jwt
+from datetime import datetime, timedelta, timezone
+from fastapi import HTTPException
+from fastapi.security import OAuth2PasswordBearer
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # Load environment variables from .env file
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+#Create a function to generate a JWT token
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from database import engine
-import models, schemas
+import models
+import schemas
+from auth import create_access_token, verify_token
+
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -17,14 +20,27 @@ def get_db():
         db.close()
 
 
+#Login route to generate JWT token
+@app.post("/login")
+def login():
+    return {
+        "access_token": create_access_token({"user": "admin"}),
+        "token_type": "bearer"
+    }    
+
+
+
 # Home route
 @app.get("/")
 def home():
     return {"message": "Welcome to the first page Home Blog API!"}
 
-#create Blog
+
+
+
+#create Blog (protected route)
 @app.post("/blogs/", response_model=schemas.BlogResponse)
-def create_blog(blog: schemas.BlogCreate, db: Session = Depends(get_db)):
+def create_blog(blog: schemas.BlogCreate, db: Session = Depends(get_db), user = Depends(verify_token)):
     new_blog = models.Blog(title=blog.title, content=blog.content, author=blog.author)
     db.add(new_blog)
     db.commit()
@@ -32,23 +48,23 @@ def create_blog(blog: schemas.BlogCreate, db: Session = Depends(get_db)):
     return new_blog
 
 
-#Read all blogs
+#Read all blogs (protected route)
 @app.get("/blogs/", response_model=list[schemas.BlogResponse])
-def get_blogs(db: Session = Depends(get_db)):
+def get_blogs(db: Session = Depends(get_db), user = Depends(verify_token)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-#Read a single blog by ID
+#Read a single blog by ID (protected route)
 @app.get("/blogs/{blog_id}", response_model=schemas.BlogResponse)
-def get_blog(blog_id: int, db: Session = Depends(get_db)):
+def get_blog(blog_id: int, db: Session = Depends(get_db), user = Depends(verify_token)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
         raise HTTPException(status_code=404, detail="Blog not found")
     return blog
 
-#update a blog by ID
+#update a blog by ID (protected route)
 @app.put("/blogs/{blog_id}", response_model=schemas.BlogResponse)
-def update_blog(blog_id: int, updated_blog: schemas.BlogCreate, db: Session = Depends(get_db)):
+def update_blog(blog_id: int, updated_blog: schemas.BlogCreate, db: Session = Depends(get_db), user = Depends(verify_token)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
         raise HTTPException(status_code=404, detail="Blog not found")
@@ -60,9 +76,9 @@ def update_blog(blog_id: int, updated_blog: schemas.BlogCreate, db: Session = De
     db.refresh(blog)
     return blog
 
-#delete a blog by ID
+#delete a blog by ID (protected route)
 @app.delete("/blogs/{blog_id}")
-def delete_blog(blog_id: int, db: Session = Depends(get_db)):
+def delete_blog(blog_id: int, db: Session = Depends(get_db), user = Depends(verify_token)):
     blog = db.query(models.Blog).filter(models.Blog.id == blog_id).first()
     if not blog:
         raise HTTPException(status_code=404, detail="Blog not found")
